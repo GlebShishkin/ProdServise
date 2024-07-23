87323e310a8e5749d74b8e113bef3149af6a4609
Gleb Shishkin <GSHishkin@inno.tech>
2024-07-23 15:08:55 +0300

    Init commit

---
+++ src/main/java/ru/stepup/prodservise/ProdServiseApplication.java
@@ -0,0 +1,13 @@
+package ru.stepup.prodservise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdServiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdServiseApplication.class, args);
+	}
+
+}
